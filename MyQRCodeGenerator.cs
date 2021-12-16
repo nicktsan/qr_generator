e21fa040e010fed9d53652d1b58530c9e61bcdd9
nicktsan <nicktsang.manhay@gmail.com>
2021-12-15 17:28:23 -0800

    Added file reading capabilities for mass qr generation

--- MyQRCodeGenerator.cs
+++ MyQRCodeGenerator.cs
@@ -9,10 +9,12 @@ namespace MyQRCodeGenerator
 {
     public class Generator
     {
-        public String url;
-        public Generator(String url)
+        public String[] url_list;
+        public Generator(String path)
         {
-            this.url = url;
+            // Read each line of the file into a string array. Each element
+            // of the array is one line of the file.
+            url_list = System.IO.File.ReadAllLines(path);
         }
         public void Generate()
         {
@@ -22,25 +24,39 @@ namespace MyQRCodeGenerator
             //Set the Error Correction Capability (ECC) Level to H (High): 30% of data bytes can be restored. 
             //Higher ECC level is needed to combat potentially harsh conditions or visual obstructions such as weather and dirt.
             ECCLevel level = ECCLevel.H;
-            QRCodeData qr = generator.CreateQrCode(url, level);
-
             SKImageInfo info = new SKImageInfo(512, 512);
             SKSurface surface = SKSurface.Create(info);
-
             SKCanvas canvas = surface.Canvas;
             //public static void Render(this SKCanvas canvas, QRCodeData data, int width, int hight, SKColor clearColor, SKColor codeColor);
-            //Creates a 512px by 512px qr code with white background and black code color.
-            canvas.Render(qr, 512, 512, SKColors.White, SKColors.Black);
+            //Creates a 512px by 512px qr code with white background and black code color for each url in the list.
+            foreach(String url in url_list)
+            {
+                if(!String.IsNullOrEmpty(url))
+                {
+                    try 
+                    {
+                        String parsedUrl = parseURL(url);
+                        QRCodeData qr = generator.CreateQrCode(url, level);
+                        canvas.Render(qr, 512, 512, SKColors.White, SKColors.Black);
 
-            using var image = surface.Snapshot();
-            using var data = image.Encode(SKEncodedImageFormat.Png, 100);
-            using var stream = File.OpenWrite(@$"qr-{level}.png");
-            data.SaveTo(stream);
-            Console.WriteLine("QR Code Generated");
+                        using var image = surface.Snapshot();
+                        using var data = image.Encode(SKEncodedImageFormat.Png, 100);
+                        using var stream = File.OpenWrite(@$"qr-{level}" + parsedUrl + @".png");
+                        data.SaveTo(stream);
+                    } catch (System.UriFormatException e)
+                    {
+                        Console.WriteLine("Found a scuffed url: " + url);
+                        Console.WriteLine(e.GetType().FullName);
+                        Console.WriteLine(e.Message);
+                    }
+                }
+            }
+            
+            Console.WriteLine("QR Codes Generated");
         }
 
-        //parse subdomain from url
-        public String parseURL()
+        //parse subdomain from url_list
+        public String parseURL(String url)
         {
             String result = url;
             var uri=new Uri(url);
--- Program.cs
+++ Program.cs
@@ -1,8 +1,5 @@
 ﻿using MyQRCodeGenerator;
 // See https://aka.ms/new-console-template for more information
 Console.WriteLine("Hello, World!");
-String testURL = "https://www.images.google.com/search?channel=fs&client=ubuntu&q=skiasharp+qrcode+white+canvas";
-Console.WriteLine("original url: " + testURL);
-Generator test = new Generator(testURL);
-//test.Generate();
-Console.WriteLine("parsed url: " + test.parseURL());
+Generator test = new Generator("/home/nicholas/Documents/qr_generator/links.txt");
+test.Generate();
